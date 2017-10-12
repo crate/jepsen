88ecd3689d8fb83ba817d77964d11902f3eb4bd3
Andrei Dan <andrei@crate.io>
2017-10-12 12:51:04 +0100

    Split the version_divergence test into it's own namespace

--- crate/src/jepsen/crate.clj
+++ crate/src/jepsen/crate.clj
@@ -20,9 +20,9 @@
             [jepsen.control.net   :as cnet]
             [cheshire.core        :as json]
             [clojure.string       :as str]
+            [clojure.java.jdbc    :as j]
             [clojure.java.io      :as io]
             [clojure.java.shell   :refer [sh]]
-            [clojure.java.jdbc :as j]
             [clojure.pprint :refer [pprint]]
             [clojure.tools.logging :refer [info warn]]
             [knossos.op           :as op])
@@ -259,119 +259,3 @@
 
             :else
             (throw e#)))))
-
-(defn client
-  ([] (client nil))
-  ([dbspec]
-   (let [initialized? (promise)]
-     (reify client/Client
-       (setup! [this test node]
-         (let [dbspec (await-client (get-node-db-spec node) node test)]
-           (when (deliver initialized? true)
-             (j/execute! dbspec
-                         ["create table if not exists registers (
-                          id     integer primary key,
-                          value  integer)"])
-             (j/execute! dbspec
-                         ["alter table registers
-                          set (number_of_replicas = \"0-all\")"]))
-           (client dbspec)))
-
-       (invoke! [this test op]
-         (let [[k v] (:value op)]
-           (timeout 500 (assoc op :type :fail, :error :timeout)
-                    (try
-                      (case (:f op)
-                        :read (->> (j/query dbspec ["select value, \"_version\"
-                                                    from registers where id = ?" k])
-                                   first
-                                   (independent/tuple k)
-                                   (assoc op :type :ok, :value))
-
-                        :write (let [res (j/execute! dbspec
-                                                     ["insert into registers (id, value)
-                                                      values (?, ?)
-                                                      on duplicate key update
-                                                      value = VALUES(value)" k v])]
-                                 (assoc op :type :ok)))
-
-                      (catch PSQLException e
-                        (cond
-                          (and (= 0 (.errorCode e))
-                               (re-find #"blocked by: \[.+no master\];" (str e)))
-                          (assoc op :type :fail, :error :no-master)
-
-                          (and (= 0 (.errorCode e))
-                               (re-find #"rejected execution" (str e)))
-                          (do ; Back off a bit
-                              (Thread/sleep 1000)
-                              (assoc op :type :info, :error :rejected-execution))
-
-                          :else
-                          (throw e)))))))
-
-       (teardown! [this test]
-         )))))      
-
-(defn multiversion-checker
-  "Ensures that every _version for a read has the *same* value."
-  []
-  (reify checker/Checker
-    (check [_ test model history opts]
-      (let [reads  (->> history
-                        (filter op/ok?)
-                        (filter #(= :read (:f %)))
-                        (map :value)
-                        (group-by :_version))
-            multis (remove (fn [[k vs]]
-                             (= 1 (count (set (map :value vs)))))
-                           reads)]
-        {:valid? (empty? multis)
-         :multis multis}))))
-
-(defn r [] {:type :invoke, :f :read, :value nil})
-(defn w []
-  (->> (iterate inc 0)
-       (map (fn [x] {:type :invoke, :f :write, :value x}))
-       gen/seq))
-
-(defn an-test
-  [opts]
-  (merge tests/noop-test
-         {:name    "crate"
-          :os      debian/os
-          :db      (db (subs (str (get opts :crate-version)) 1))
-          :client  (client)
-          :checker (checker/compose
-                     {:multi    (independent/checker (multiversion-checker))
-                      :timeline (timeline/html)
-                      :perf     (checker/perf)})
-          :concurrency 100
-          :nemesis (nemesis/partition-random-halves)
-          :generator (->> (independent/concurrent-generator
-                            10
-                            (range)
-                            (fn [id]
-                              (->> (gen/reserve 5 (r) (w)))))
-                          (gen/nemesis
-                            (gen/seq (cycle [(gen/sleep 120)
-                                             {:type :info, :f :start}
-                                             (gen/sleep 120)
-                                             {:type :info, :f :stop}])))
-                          (gen/time-limit 360))}
-         opts))
-
-(def opt-spec
-  "Additional command line options"
-  [[nil "--crate-version CRATE_VERSION" "CrateDB Version, e.g. 2.0.7-1~jessie_all"
-    :parse-fn keyword
-    :missing  (str "Missing --crate-version CRATE_VERSION")
-    ]])
-
-(defn -main [& args]
-  "Handles command line arguments. Can either run a test, or a web service
-  browsing results."
-  (cli/run! (merge (cli/single-test-cmd {:test-fn   an-test
-                                         :opt-spec  opt-spec})
-                   (cli/serve-cmd))
-            args))
