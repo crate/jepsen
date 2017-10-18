72188c9f96691b889a40393d985a9cba00e536ef
Marios Trivyzas <marios@crate.io>
2017-10-18 15:22:00 +0200

    Use a combined exit status for all tests

--- crate/runall.sh
+++ crate/runall.sh
@@ -1,7 +1,11 @@
 #! /bin/sh
 
+EXIT_STATUS=0
 for test in "dirty-read" "lost-updates" "version-divergence"
 do
     lein run test --test ${test} --concurrency 2n --crate-version $1
+    EXIT_STATUS=$(expr ${EXIT_STATUS} + $?)
     sleep 15
 done
+
+exit ${EXIT_STATUS}
