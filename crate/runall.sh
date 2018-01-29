d946b1854a4d0158a2814cfc4338ca318defb44f
Andrei Dan <andrei@crate.io>
2018-01-29 18:52:24 +0000

    Grab dmesg output after running tests

--- crate/runall.sh
+++ crate/runall.sh
@@ -9,6 +9,13 @@ do
     EXIT_STATUS=$(expr ${EXIT_STATUS} + ${TEST_EXIT_STATUS})
     STATUS="$STATUS \n Test ${test} exited with status ${TEST_EXIT_STATUS}"
     sleep 15
+    for i in $(seq 1 5)
+    do
+	    nodeName="n$i"
+        test_dir=$(ls -d store/[${test}]*)
+        latest_test_dir=$(ls -Art store/${test}* | tail -n 1)
+        ssh root@"$nodeName" 'dmesg' > "${test_dir}/${latest_test_dir}/${nodeName}_dmesg.out"
+    done
 done
 
 echo $STATUS
